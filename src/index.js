b5ad265157aeabe5e3925add9d7c597423b1d04b
Benjamin Franzke <bfr@qbus.de>
2020-07-01 13:15:53 +0200

    Fix typo in previous release

--- src/index.js
+++ src/index.js
@@ -149,7 +149,7 @@ Consent.prototype.createDialog = function() {
             // Reopen the modal if it wasn't submitted.
             // This is ugly, but there is no other way to prevent
             // the modal from being closed by pressing the 'Escape' key.
-            if (!this.storage.get('data-consent')) {
+            if (!self.storage.get('data-consent')) {
                 self.dialog.showModal();
             }
             break;
