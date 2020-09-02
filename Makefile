13a4ad5d23910e8041f6434a123a31b6314e57bd
Benjamin Franzke <bfr@qbus.de>
2020-09-02 15:21:40 +0200

    Make use of rollup-plugin-copy to install node dependencies
    npm install rollup-plugin-copy -D

--- Makefile
+++ Makefile
@@ -4,7 +4,6 @@ upload:
 build:
 	npm run build
 	npm run rollup
-	cp node_modules/es6-promise/dist/es6-promise.auto.min.js Resources/Public/JavaScript/
 	npm run cleancss
 	date +%s | tr -d '\n' > buildtime
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 // rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
+import copy from 'rollup-plugin-copy'
 
 export default {
     input: 'src/index.js',
@@ -36,6 +37,13 @@ export default {
         resolve({
             mainFields: ['module', 'main'],
             modulesOnly: true
+        }),
+        copy({
+            targets: [
+                { src: 'node_modules/es6-promise/dist/es6-promise.auto.min.js', dest: 'Resources/Public/JavaScript/' }
+            ],
+            verbose: true,
+            copyOnce: true
         }),
     ],
     external: [ 'es6-promise' ],
